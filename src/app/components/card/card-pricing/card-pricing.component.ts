eaa20205a67e7b7677840730a72bd2c50c6babe9
yesgabio <oliveirasgabi@gmail.com>
2023-04-29 18:50:28 -0300

    first commit

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gameTipe: string = ""
+  @Input()
+  gamePrice: string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
